75e78a85dab4022433fe39292d95bc0a188b57b5
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-08-13 15:24:02 +0900

    [Score : 42,138] 時間いっぱい独立に構成し直し、最良のものを提出。

--- b.cpp
+++ b.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const double TIME_LIMIT = 2.9;
+struct Timer {
+    clock_t start;
+    Timer() { reset(); }
+    void reset() { start = clock(); }
+    double get() { return (double)(clock() - start) / CLOCKS_PER_SEC; }
+};
+
 struct MoveAction {
     int before_row, before_col, after_row, after_col;
     MoveAction(int before_row, int before_col, int after_row, int after_col) : 
@@ -25,22 +33,40 @@ struct Solver {
 
     int N, K;
     int action_count_limit;
-    // mt19937 engine;
+    random_device rd;
+    mt19937 engine{rd()};
     vector<vector<int>> field;
 
-    Solver(int N, int K, const vector<vector<int>> &field, int seed = 0) :
-        N(N), K(K), action_count_limit(K * 100), field(field) 
-    {
-        // engine.seed(seed);
+    Solver(int N, int K, const vector<vector<int>> &field) :
+        N(N), K(K), action_count_limit(K * 100), field(field) {}
+
+    bool can_move(int &x, int &y, int &dir) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (inside(nx, ny)) {
+            return field[nx][ny] == 0;
+        }
+        return false;
     }
 
     vector<MoveAction> move(int move_limit = -1) {
         vector<MoveAction> moves;
-        if (move_limit == -1) move_limit = K * 50;
+        if (move_limit == -1) {
+            move_limit = K * 50;
+        }
 
-        // for (int i = 0; i < move_limit; i++) {
-
-        // }
+        for (int i = 0; i < move_limit; i++) {
+            int x = engine() % N;
+            int y = engine() % N;
+            int dir = engine() % 4;
+            if (field[x][y] > 0 && can_move(x, y, dir)) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                swap(field[x][y], field[nx][ny]);
+                moves.emplace_back(x, y, nx, ny);
+                action_count_limit--;
+            }
+        }
 
         return moves;
     }
@@ -128,6 +154,65 @@ void init() {
 void simulate() {
 }
 
+struct UnionFind {
+    map<pair<int,int>, pair<int, int>> parent;
+    UnionFind() :parent() {}
+
+    pair<int, int> find(pair<int, int> x) {
+        if (parent.find(x) == parent.end()) {
+            parent[x] = x;
+            return x;
+        } else if (parent[x] == x) {
+            return x;
+        } else {
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+    }
+
+    void unite(pair<int, int> x, pair<int, int> y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[x] = y;
+        }
+    }
+};
+
+int calc_score(int N, vector<vector<int>> field, const Result &res) {
+    for (MoveAction m : res.move) {
+        assert(field[m.before_row][m.before_col] > 0);
+        assert(field[m.after_row][m.after_col] == 0);
+        swap(field[m.before_row][m.before_col], field[m.after_row][m.after_col]);
+    }
+    UnionFind uf;
+    for (ConnectAction c : res.connect) {
+        pair<int, int> p1(c.c1_row, c.c1_col), p2(c.c2_row, c.c2_col);
+        uf.unite(p1, p2);
+    }
+
+    vector<pair<int, int>> computers;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (field[i][j] <= 0) continue;
+            computers.emplace_back(i, j);
+        }
+    }
+
+    int score = 0;
+    for (int i = 0; i < (int)computers.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            pair<int, int> c1 = computers[i];
+            pair<int, int> c2 = computers[j];
+            if (uf.find(c1) == uf.find(c2)) {
+                score += (field[c1.first][c1.second] == field[c2.first][c2.second]) ? 1 : -1;
+            }
+        }
+    }
+
+    return max(score, 0);
+}
+
 void output(const Result &res) {
     cout << res.move.size() << endl;
     for (MoveAction m : res.move) {
@@ -148,9 +233,30 @@ void solve() {
 
     Solver s(N, K, field);
     Result res = s.solve();
+    int max_score = calc_score(N, field, res);
+
+    Timer timer;
+    int loop_count = 0;
+    int update_count = 0;
+    while (timer.get() < TIME_LIMIT) {
+        loop_count++;
+        Solver t(N, K, field);
+        Result now = t.solve();
+        int now_score = calc_score(N, field, now);
+        if (now_score > max_score) {
+            update_count++;
+            max_score = now_score;
+            res = now;
+        }
+    }
     // init();
     // simulate();
+
     output(res);
+
+    cerr << "Score  : " << max_score << endl;
+    cerr << "Loop   : " << loop_count << endl;
+    cerr << "Update : " << update_count << endl;
 }
 
 int main(){
